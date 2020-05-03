5cfacbd1f944af344e65854385bfe0cea258123b
Karthik Sankaran <contact@karthiksankaran.com>
2020-05-03 13:58:40 -0400

    Added ArcGIS map

--- src/App.vue
+++ src/App.vue
@@ -60,36 +60,10 @@
         <v-row
           justify="center"
           align="center"
+          class="fill-height"
         >
-          <v-col class="shrink">
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  :href="source"
-                  icon
-                  large
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-code-tags</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  icon
-                  large
-                  href="https://codepen.io/johnjleider/pen/QewYYx"
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-codepen</v-icon>
-                </v-btn>
-              </template>
-              <span>Codepen</span>
-            </v-tooltip>
+          <v-col class="fill-height">
+            <div id="viewDiv" style="width:100%;" class="fill-height"></div>
           </v-col>
         </v-row>
       </v-container>
